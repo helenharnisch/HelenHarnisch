2ece16019d8b985f9918c2c6db1f92cbb16b9c3b
helenharnisch <harnischvioleta@gmail.com>
2018-03-27 03:08:18 -0300

    Proceso Projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,11 @@
 window.sr = ScrollReveal({ reset: true, mobile: true });
 
 sr.reveal('.reveal', { duration: 3000 });
+
+$(window).scroll(function() {
+  if ($("#menu").offset().top > 56) {
+    $("#menu").addClass("down");
+  } else {
+    $("#menu").removeClass("down");
+  }
+});
